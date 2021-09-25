92850c73c929537cc63ee4df0c08fb5582072b5b
Baruch Rotbart <BaruchRotbart@WesleyHills-MacBook-Pro.local>
2021-09-25 12:00:23 -0500

    stuff

--- work.py
+++ work.py
@@ -1,3 +1,19 @@
-print("Hello World!")
+class ParkingGarage():
 
-print('wtvr')
+    def __init__(self, tickets, parkingSpaces, currentTicket = {} ):
+        self.tickets = tickets
+        self.parkingspaces = parkingSpaces
+        self.currentticket = currentTicket
+
+
+    def taketicket(self):
+        pass
+
+    def payForParking(self):
+        pass
+
+    def leaveGarage(self):
+        pass
+
+def driver():
+    pass
