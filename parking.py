201b9d67c94a50813ff79f13a85285da74ac9c7e
Redwan Diriye <ridwanmohamedco@outlook.com>
2021-09-26 17:05:46 -0500

    Almost Done!

---
+++ parking.py
@@ -0,0 +1,57 @@
+# MAIN CLASS
+
+class Parking():
+
+
+    price = 4.99
+    
+    
+
+    def __init__(self):
+        self.tickets = [1,2,3]
+        self.spaces = [1,2,3]
+        self.current_ticket = {}
+
+
+
+    def take_ticket(self):
+        take_ticket = input('Would you like to park here Y/N?').lower()
+
+        if take_ticket == 'n':
+            print('Cya around')
+        if take_ticket == 'y':
+           self.current_ticket[self.tickets.pop()] = False
+           print(self.current_ticket) 
+        else:
+            print('Invaid input')
+        
+
+
+    def payforparking(self,price):
+       pay = int(input('What ticket do you have?'))
+       pass
+
+        
+   
+
+
+    def leave_garage(self):
+        pass
+
+
+parking = Parking()
+parking.take_ticket()       
+            
+
+
+
+        
+
+      
+
+    
+
+
+
+
+    
